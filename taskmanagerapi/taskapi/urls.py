4932952f679b861b94d3a3e1b93bc90814664940
Trevor Sherwood <sherwoodwt@gmail.com>
2017-05-13 21:05:30 -0700

    Added comment views

--- taskmanagerapi/taskapi/urls.py
+++ taskmanagerapi/taskapi/urls.py
@@ -1,11 +1,15 @@
 from django.conf.urls import url
 
 from .views import (
+    comments,
+    comments_by_id,
     tasks,
-    task_by_id
+    tasks_by_id
 )
 
 urlpatterns = [
     url(r'^tasks/$', tasks),
-    url(r'^tasks/(?P<task_id>[0-9]+)/$', task_by_id)
+    url(r'^tasks/(?P<task_id>[0-9]+)/$', tasks_by_id),
+    url(r'^comments/$', comments),
+    url(r'^comments/(?P<comment_id>[0-9]+)/$', comments_by_id),
 ]
--- taskmanagerapi/taskapi/views.py
+++ taskmanagerapi/taskapi/views.py
@@ -7,8 +7,8 @@ from rest_framework import status
 from rest_framework.parsers import JSONParser
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from .models import Task
-from .serializers import TaskSerializer
+from .models import Task, Comment
+from .serializers import TaskSerializer, CommentSerializer
 
 
 # Create your views here.
@@ -32,7 +32,7 @@ def tasks(request):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 @api_view(['GET', 'PUT', 'DELETE'])
-def task_by_id(request, task_id):
+def tasks_by_id(request, task_id):
     """
     Functions for Tasks with an id
     """
@@ -58,4 +58,47 @@ def task_by_id(request, task_id):
         task.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
 
+@api_view(['GET', 'POST'])
+def comments(request):
+    """
+    Functions for Comments with no id
+    """
+    if request.method == 'GET':
+        comments = Comment.objects.all()
+        serializer = CommentSerializer(comments, many=True)
+        return Response(serializer.data)
 
+    elif request.method == 'POST':
+        data = JSONParser().parse(request)
+        serializer = CommentSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def comments_by_id(request, comment_id):
+    """
+    Functions for Comments with an id
+    """
+
+    try:
+        comment = Comment.objects.get(pk=comment_id)
+    except:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        serializer = CommentSerializer(comment)
+        return Response(serializer.data)
+    
+    elif request.method == 'PUT':
+        data = JSONParser().parse(request)
+        serializer = CommentSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == 'DELETE':
+        comment.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
